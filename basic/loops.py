838b224f2cebc6be2c59a6d3bf7113b9eea15aab
unkownakash <akashmandy3609@gmail.com>
2025-05-13 18:32:39 +0530

    lopps

---
+++ basic/loops.py
@@ -0,0 +1,30 @@
+# while loop
+plc=0
+while(plc<3):
+ plc=plc+1
+ print("hello JI kaise Ho")
+# we can use else condition in while loop 
+plc=0
+while(plc<3):
+ plc=plc+1
+ print("hello JI kaise Ho")
+else:
+ print("The END")
+# For Loop
+"""
+syntax
+for iterator_var in sequence:
+    statements(s)
+example
+for i in range(0,5):
+ print(i)
+"""
+for i in range(0,10):
+    print(i)
+# you can also print list of word 
+color ={"1","2",3,4}
+for i in color:
+    print(i)
+fruits= "apple"
+for i in fruits:
+    print(i)    
---
+++ basic/tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+color ={"1","2",3,4}
+for i in color:
+    print(i)
